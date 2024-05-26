096c94565092235e3cedd83f068af601eb50bb53
fenriskanalytics <brandon@epifronlegacypartners.com>
2024-05-26 13:46:56 -0400

    f

--- pages/schedule.jsx
+++ pages/schedule.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { useEffect } from "react";
 import { PageParamsProvider as PageParamsProvider__ } from "@plasmicapp/react-web/lib/host";
 import GlobalContextsProvider from "../components/plasmic/copy_of_halcyon_studios/PlasmicGlobalContextsProvider";
 import { UnnamedGlobalGroupOfVariantsContext } from "../components/plasmic/copy_of_halcyon_studios/PlasmicGlobalVariant__UnnamedGlobalGroupOfVariants";
@@ -9,61 +8,20 @@ import { useRouter } from "next/router";
 function Schedule() {
   const router = useRouter();
 
-  const injectScript = () => {
-    const tenantName = 'halcyonstudio';
-    const scriptContent = `
-      (function () {
-        var TENANT_NAME = '${tenantName}';
-        var d = document;
-        var sA = ['polyfills', 'js'];
-
-        for (var i = 0; i < sA.length; i++) {
-          var s = d.createElement('script');
-          s.src = 'https://' + TENANT_NAME + '.marianaiframes.com/' + sA[i];
-          s.setAttribute('data-timestamp', +new Date());
-          (d.head || d.body).appendChild(s);
-        }
-      })();
-    `;
-    const scriptElement = document.createElement('script');
-    scriptElement.innerHTML = scriptContent;
-    document.body.appendChild(scriptElement);
-
-    return () => {
-      if (scriptElement.parentNode) {
-        scriptElement.parentNode.removeChild(scriptElement);
-      }
-    };
-  };
-
-  useEffect(() => {
-    injectScript();
-
-    const handleRouteChange = () => {
-      injectScript();
-    };
-
-    router.events.on('routeChangeComplete', handleRouteChange);
-
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router]);
-
   return (
     <UnnamedGlobalGroupOfVariantsContext.Provider value={undefined}>
       <GlobalContextsProvider>
         <PageParamsProvider__
-          route={useRouter()?.pathname}
-          params={useRouter()?.query}
-          query={useRouter()?.query}
+          route={router.pathname}
+          params={router.query}
+          query={router.query}
         >
           <PlasmicSchedule
             marianaScheduleCode={{
               props: {
                 children: (
                   <div data-mariana-integrations="/schedule/daily">
-                    {props.children}
+                    {/* Additional content or children can be placed here */}
                   </div>
                 ),
               },
@@ -71,30 +29,6 @@ function Schedule() {
           />
         </PageParamsProvider__>
       </GlobalContextsProvider>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function () {
-              var TENANT_NAME = 'halcyonstudio';
-              var d = document;
-              var sA = ['polyfills', 'js'];
-
-              for (var i = 0; i < sA.length; i++) {
-                var s = d.createElement('script');
-                s.src = 'https://' + TENANT_NAME + '.marianaiframes.com/' + sA[i];
-                s.setAttribute('data-timestamp', +new Date());
-                (d.head || d.body).appendChild(s);
-              }
-            })();
-          `,
-        }}
-      ></script>
-      <noscript>
-        Please enable JavaScript to view the
-        <a href="https://marianatek.com/?ref_noscript" rel="nofollow">
-          Web Integrations by Mariana Tek.
-        </a>
-      </noscript>
     </UnnamedGlobalGroupOfVariantsContext.Provider>
   );
 }
