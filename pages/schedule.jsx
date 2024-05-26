b1ab1747dab00e71cec9573c395f3782d7f40bc6
fenriskanalytics <brandon@epifronlegacypartners.com>
2024-05-26 13:28:27 -0400

    t

--- pages/schedule.jsx
+++ pages/schedule.jsx
@@ -10,15 +10,24 @@ function Schedule() {
   const router = useRouter();
 
   const injectScript = () => {
+    const tenantName = 'halcyonstudio';
+    const scriptContent = `
+      (function () {
+        var TENANT_NAME = '${tenantName}';
+        var d = document;
+        var sA = ['polyfills', 'js'];
+
+        for (var i = 0; i < sA.length; i++) {
+          var s = d.createElement('script');
+          s.src = 'https://' + TENANT_NAME + '.marianaiframes.com/' + sA[i];
+          s.setAttribute('data-timestamp', +new Date());
+          (d.head || d.body).appendChild(s);
+        }
+      })();
+    `;
     const scriptElement = document.createElement('script');
-    scriptElement.src = 'https://example.com/mariana-integration.js'; // Replace with actual script URL
-    scriptElement.async = true;
-    scriptElement.onload = () => {
-      if (window.MarianaIntegration) {
-        window.MarianaIntegration.init('/account');
-      }
-    };
-    document.body.appendChild(scriptElement);
+    scriptElement.innerHTML = scriptContent;
+    document.head.appendChild(scriptElement);
   };
 
   useEffect(() => {
@@ -49,6 +58,35 @@ function Schedule() {
                 <div data-mariana-integrations="/account">
                   {content}
                 </div>
+              ),
+            }}
+            halcyonFooterBottom={{
+              wrap: (content) => (
+                <>
+                  {content}
+                  <script>
+                    {`
+                      (function () {
+                        var TENANT_NAME = 'halcyonstudio';
+                        var d = document;
+                        var sA = ['polyfills', 'js'];
+
+                        for (var i = 0; i < sA.length; i++) {
+                          var s = d.createElement('script');
+                          s.src = 'https://' + TENANT_NAME + '.marianaiframes.com/' + sA[i];
+                          s.setAttribute('data-timestamp', +new Date());
+                          (d.head || d.body).appendChild(s);
+                        }
+                      })();
+                    `}
+                  </script>
+                  <noscript>
+                    Please enable JavaScript to view the
+                    <a href="https://marianatek.com/?ref_noscript" rel="nofollow">
+                      Web Integrations by Mariana Tek.
+                    </a>
+                  </noscript>
+                </>
               ),
             }}
           />
