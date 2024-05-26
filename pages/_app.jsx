c8df0a84e326994e06a64ed60f259c819071c7ef
fenriskanalytics <brandon@epifronlegacypartners.com>
2024-05-25 20:22:04 -0400

    t

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,45 @@
-import '@/styles/globals.css'
-import { PlasmicRootProvider } from "@plasmicapp/react-web";
-import Head from "next/head";
+// pages/_app.js
+import React from 'react';
+import App from 'next/app';
+import Head from 'next/head';
 
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <PlasmicRootProvider Head={Head}>
-      <Component {...pageProps} />
-    </PlasmicRootProvider>
-  );
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <>
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function () {
+                  // Set tenant name here
+                  var TENANT_NAME = 'your-tenant-name'; // Replace 'your-tenant-name' with your actual tenant name
+
+                  var d = document;
+                  var sA = ['polyfills', 'js'];
+
+                  for (var i = 0; i < sA.length; i++) {
+                    var s = d.createElement('script');
+                    s.src = 'https://' + TENANT_NAME + '.marianaiframes.com/' + sA[i];
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                  }
+                })();
+              `,
+            }}
+          />
+        </Head>
+        <Component {...pageProps} />
+        <noscript>
+          Please enable JavaScript to view the
+          <a href="https://marianatek.com/?ref_noscript" rel="nofollow">
+            Web Integrations by Mariana Tek.
+          </a>
+        </noscript>
+      </>
+    );
+  }
 }
+
+export default MyApp;
