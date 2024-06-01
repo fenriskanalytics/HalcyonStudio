8bed294d1b7a58eb73ccc2369b274114636c2bfc
fenriskanalytics <brandon@epifronlegacypartners.com>
2024-05-31 21:01:37 -0400

    n

--- pages/memberships.jsx
+++ pages/memberships.jsx
@@ -10,22 +10,20 @@ import { useRouter } from "next/router";
 function Memberships() {
   const router = useRouter();
 
-  // Use PlasmicMemberships to render this component as it was
-  // designed in Plasmic, by activating the appropriate variants,
-  // attaching the appropriate event handlers, etc.  You
-  // can also install whatever React hooks you need here to manage state or
-  // fetch data.
-  //
-  // Props you can pass into PlasmicMemberships are:
-  // 1. Variants you want to activate,
-  // 2. Contents for slots you want to fill,
-  // 3. Overrides for any named node in the component to attach behavior and data,
-  // 4. Props to set on the root node.
-  //
-  // By default, PlasmicMemberships is wrapped by your project's global
-  // variant context providers. These wrappers may be moved to
-  // Next.js Custom App component
-  // (https://nextjs.org/docs/advanced-features/custom-app).
+  React.useEffect(() => {
+    const handleRouteChange = () => {
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <UnnamedGlobalGroupOfVariantsContext.Provider value={undefined}>
       <GlobalContextsProvider>
